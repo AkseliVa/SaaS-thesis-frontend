d34c2dfc368e92f1562d02fcaf485a51ee40b44f
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2025-06-19 11:00:05 +0300

    added dashboard cards

---
+++ src/components/DashboardCard.tsx
@@ -0,0 +1,26 @@
+import { Card, CardActionArea, CardMedia, CardContent, Typography } from "@mui/material";
+
+function DashboardCard({ title, description, image }: { title: string, description: string, image: string }) {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={image}
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
+
+export default DashboardCard;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,27 @@
+import DashboardCard from "../components/DashboardCard";
+import employees from "../assets/images/employees.jpg";
+import projects from "../assets/images/projects.jpg";
+import calendar from "../assets/images/calendar.jpg";
+
 function Dashboard() {
     return (
         <>
             <h1>Dashboard</h1>
+            <DashboardCard 
+                title="Employees" 
+                description="View, add, edit and delete your employees here"
+                image={employees}
+            />
+            <DashboardCard
+                title="Projects"
+                description="View, add, edit and delete your projects here"
+                image={projects}
+            />
+            <DashboardCard
+                title="Calendar"
+                description="View your calendar here"
+                image={calendar}
+            />
         </>
     )
 }
